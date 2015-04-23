4c5553cabe6a17af5e64d36d01e62eea7617237f
MoritzNebel <moritz.ne@t-online.de>
2015-04-23 07:56:14 +0200

    Revert "Space War"
    This reverts commit b2fec08539ffbdb99b99285ded3cd3a9ecfaf8cb.

---
+++ 2D Space Shooter/Assets/Scripts/MenuLoader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuLoader : MonoBehaviour {
+
+
+
+    void OnTriggerEnter2D(Collider2D PlayerShip)
+    {
+
+
+        if (PlayerShip.tag == "FinalGoal")
+        {
+            if (GameObject.FindGameObjectsWithTag("Goal").Length == 0)
+            {
+                Application.LoadLevel("Menu");
+                TimeDamageHandler.live = 0;
+
+            }
+
+
+        }
+
+
+
+
+    }
+}
